a373ab95c8f199f41cd8755ae9d2d1bf326db35c
René Winkler <info@rewiso.ch>
2019-07-20 23:16:38 +0200

    dist dir

--- app.js
+++ app.js
@@ -14,9 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
+// app.use('/', indexRouter);
 app.use('/bikes', bikesRouter);
 
 module.exports = app;
